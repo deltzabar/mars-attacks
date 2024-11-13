7521dd38c802097c97aed9cb26ab26a97fad462a
Sofia Wolf <sofiakwolf@gmail.com>
2024-11-13 18:14:08 +0000

    Add OMAC logic

--- mac-implement.py
+++ mac-implement.py
@@ -40,21 +40,63 @@ def u(L):
     if binL[1] == 0:
         print("starts with 0")
         shiftL = intL << 1
-        byteL = shiftL.to_bytes(17, byteorder='big')
-        print("post shift: "+str(shiftL)+" and as hex: " + byteL.hex())
-        return byteL #WHY won't it accept 16??? i don't understand
+        return shiftL
      # else, perform <<1
     else:
         print("starts with 1")
         shiftL = (intL << 1) ^ C
-        byteL = shiftL.to_bytes(17, byteorder='big')
-        print("post shift: "+str(shiftL)+" and as hex: " + byteL.hex())
-        return byteL 
+        #print(hex(shiftL)) 
+        return shiftL
    
     # then xor with constant
+
+def OMAC(k1, k2, k3, m):
+    ##TODO: message will need to be converted to byte to work## <- or does it? am i passing in a bytes an assuming it'll all work? confirm
+    block, rest = peel(m)
+    #check if last block
+    if rest != []:
+        #create first Enc block
+        enc1 = blockEncrypt(k1, block)
+        intEnc = int.from_bytes(enc1, byteorder='big')
+        while rest != []: #loop while there are still more block to come
+            block, rest = peel(rest)
+            if rest == []: #if nothing left, do lastblock procedures
+                lastblock(k1, k2, k3, block, enc1)
+            else:
+                intBlock = int.from_bytes(block, byteorder='big')
+                xor = intEnc ^ intBlock
+                enc1 = blockEncrypt(k1, xor)
+                intEnc = int.from_bytes(enc1, byteorder='big')
+                #perform encryption
+    #if yes
+    else:
+        lastblock(k1, k2, k3, block, [])
+
+
+def lastblock(k1, k2, k3, m, enc1):
+    ##TODO:Check types match expected##
+    if enc1 == []:
+        if len(m) == 16:
+            last = k2 ^ m
+            return blockEncrypt(k1, last)
+        else:
+            ##apply padding
+            last = k3 ^ m
+            return blockEncrypt(k1, last)
+    else:
+        if len(m) == 16:
+            last = m ^ enc1 ^ k2
+            return blockEncrypt(k1, last)
+        else:
+            ##run padding
+            last = m ^ enc1 ^ k3
+            return blockEncrypt(k2, last)
     
+def OMACverif():
+    print("placeholder!")
 
-
+def OMACattack1():
+    print("placeholder!")
 
 def main():
     setup()
@@ -64,14 +106,15 @@ def main():
     #print(ctxt)
     #output = blockDecrypt(key, ctxt)
     #print(output)
-    message = "12345678901234567890"
-    first, second = peel(message, 16)
-    toEnc = str.encode(first)
+    #message = "12345678901234567890"
+    #first, second = peel(message, 16)
+    #toEnc = str.encode(first)
     #print(blockDecrypt(key, blockEncrypt(key, toEnc)))
-    omacKeyGen(key)
+    k1, k2, k3 = omacKeyGen(key)
 
 
 
 main()
 
+##k2 and k3 DONT HAVE TO BE BYTES !!! CAN STAY AS INTS/HEX !!!!
 
